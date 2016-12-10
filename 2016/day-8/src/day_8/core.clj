0175feb3605c856201b5192b426bb12f598d84e8
John McConnell <john_mcconnell@apple.com>
2016-12-09 20:58:54 -0800

    implemented day 8 2016 part 1

--- 2016/day-8/src/day_8/core.clj
+++ 2016/day-8/src/day_8/core.clj
@@ -10,40 +10,104 @@
     constantly
     (map (range 0 r-cnt))))
 
-(defn merge-col
-  [mx-2 r-idx]
-  (if-let [row-2 (nth mx-2 r-idx nil)]
-    (fn
-      [c-idx v1]
-      (if-let [v2 (nth row-2 c-idx nil)]
-        v2
-        v1))
-    (fn
-      [c-idx v1]
-      v1)))
+(defn ->vec
+  [mx]
+  (->>
+    mx
+    (map vec)
+    vec))
 
-(defn merge-row
-  [mx-2]
-  (fn
-    [r-idx row]
-    (map-indexed
-      (merge-col mx-2 r-idx) row)))
+
+(defn turn-on-with
+  [mx [x y v]]
+  (->
+    mx
+    ->vec
+    (update-in [x y] (constantly v))))
+
+(defn turn-on
+  [mx [x y]]
+  (turn-on-with mx [x y 1]))
 
 (defn merge-mx
-  [mx mx-2]
-  (map-indexed (merge-row mx-2) mx))
+  [mx s-r-cnt s-c-cnt]
+  (let [r-cnt (min (count mx) s-r-cnt)
+        c-cnt (min (count (first mx)) s-c-cnt)]
+    (->>
+      (for [x (range 0 r-cnt)
+            y (range 0 c-cnt)]
+        [x y])
+      (reduce turn-on mx))))
 
+(defn rotate-row
+  [mx idx step]
+  (let [nl (->vec mx)
+        row-size (count (first mx))
+        row (->
+              nl
+              (nth idx)
+              cycle)
+        shifted (->>
+                 row
+                 (drop (mod (- step) row-size))
+                 (take row-size))]
+    (assoc nl idx shifted)))
 
-(defn rect-a-b
+(defn col-pos-val
+  [mx c-idx step]
+  (fn
+    [r-idx]
+    (let [col-size (count mx)
+          next-r-idx (mod (- r-idx step) col-size)
+          v (->
+              mx
+              ->vec
+              (get-in [next-r-idx c-idx]))]
+          [r-idx c-idx v])))
+
+(defn rotate-col
+  [mx idx step]
+  (->>
+    mx
+    count
+    (range 0)
+    (map (col-pos-val mx idx step))
+    (reduce turn-on-with mx)))
+
+(def rect merge-mx)
+
+(defn instr->
+  [[fst snd _ fth] k]
+  (condp = k
+    :r (-> (clojure.string/split fst #"x") (nth 1) Integer.)
+    :c (-> (clojure.string/split fst #"x") first Integer.)
+    :idx (-> (clojure.string/split snd #"=") (nth 1) Integer.)
+    :step (Integer. fth)))
+
+(defn instr-dispatch
   [mx s]
-  (let [[r-cnt c-cnt] (->>
-                        (clojure.string/split s #"x")
-                        (map #(Integer. %)))
-        fill (fill-mx 1 r-cnt c-cnt)]
-    (merge-mx mx fill)))
-
+  (let [parts (clojure.string/split s #"\s+")
+        cmd (first parts)
+        args (rest parts)]
+    ;(println mx cmd args)
+    (condp = cmd
+      "rect" (rect mx (instr-> args :r) (instr-> args :c))
+      "rotate" (condp = (first args)
+                 "row"
+                 (rotate-row mx (instr-> args :idx) (instr-> args :step))
+                 "column"
+                 (rotate-col mx (instr-> args :idx) (instr-> args :step))))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (->>
+    *in*
+    slurp
+    clojure.string/split-lines
+    (reduce
+      instr-dispatch (fill-mx 0 6 50))
+    flatten
+    (filter #(= 1 %))
+    count
+    println))
