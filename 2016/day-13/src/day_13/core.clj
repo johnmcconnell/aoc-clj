15cb6a407ee727adbfd4be00f68da97bfe6ba9a4
John McConnell <john_mcconnell@apple.com>
2016-12-14 17:01:35 -0800

    adding valid positions

--- 2016/day-13/src/day_13/core.clj
+++ 2016/day-13/src/day_13/core.clj
@@ -21,12 +21,47 @@
 
 (defn next-positions
   [visited x y]
+  (let [all-visited (conj visited [x y])]
+    (->>
+      [[(st - x) y] [(st + x) y]
+       [x (st - y)] [x (st + y)]]
+      (remove #(all-visited %))
+      distinct
+      set)))
+
+(defn next-valid-positions
+  [visited seed x y]
   (->>
-    [[(st - x) y] [(st + x) y]
-    [x (st - y)] [x (st + y)]]
-    (remove #(visited %))
-    distinct
+    (next-positions visited x y)
+    (remove
+      (fn
+        [[x y]]
+        (=
+         (type-of-pos x y seed) :wall)))
     set))
+
+(defn breadth-first-iter
+  [{:keys [distances d seed pos end] :as state}]
+  (if (= pos end)
+    (assoc state :done true)
+    (assoc state :done true)))
+    ;(let [nxts (next-valid-positions seed distances)]))
+
+(defn mindist
+  [seed pos end]
+  (let [distances (->>
+                    (iterate
+                      breadth-first-iter
+                      {:distances {pos 0}
+                       :d 0
+                       :seed seed
+                       :pos pos
+                       :end end
+                       :done false})
+                    (drop-while #(not (% :done)))
+                    first
+                    :distances)]
+    (distances end)))
 
 (defn -main
   "I don't do a whole lot."
