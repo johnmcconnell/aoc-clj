b625bf85385b2817d7ff7c1e40dc932d64bb68c2
John McConnell <john_mcconnell@apple.com>
2016-12-15 15:22:02 -0800

    implemented day 13 part 2 2016

--- 2016/day-13/src/day_13/core.clj
+++ 2016/day-13/src/day_13/core.clj
@@ -52,7 +52,7 @@
   [{:keys [visited d seed next-pts end] :as state}]
   (cond
     (some #{end} next-pts) (assoc state :done true)
-    (= d 10000000) (assoc state :done true)
+    (= d 50) (assoc state :done true)
     :else (let [{next-pts :next-pts
                  visited :visited}
                 (reduce
@@ -82,9 +82,25 @@
     first
     :d))
 
+(defn mindist-2
+  [seed pos end]
+  (->>
+    (iterate
+      breadth-first-iter
+      {:visited #{pos}
+       :d 0
+       :seed seed
+       :next-pts [pos]
+       :end end
+       :done false})
+    (drop-while #(not (% :done)))
+    first
+    :visited
+    count))
+
 (defn -main
   "I don't do a whole lot."
   [& args]
   (->
-    (mindist 1350 [1 1] [31 39])
+    (mindist-2 1350 [1 1] [31 39])
     println))
