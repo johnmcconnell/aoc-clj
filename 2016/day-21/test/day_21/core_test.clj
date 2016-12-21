f2978d808d1780bf6d68b0c3e044ff6ed11096b8
John McConnell <john_mcconnell@apple.com>
2016-12-20 23:18:18 -0800

    part 2

--- 2016/day-21/test/day_21/core_test.clj
+++ 2016/day-21/test/day_21/core_test.clj
@@ -36,6 +36,7 @@
     (is (=
          "abdec" (apply str ((move-pos ["3" nil nil "0"]) "bdeac"))))))
 
+; 333
 (deftest scramble-test
   (testing "abcde"
     (let [s "abcde"
@@ -48,3 +49,37 @@
                   "rotate based on position of letter b"
                   "rotate based on position of letter d"]]
       (is (= "decab" (scramble s instrs))))))
+
+; 309
+(deftest unscramble-test
+  (testing "decab using letter d"
+    (let [s "decab"
+          instrs ["rotate based on position of letter d"]]
+      (is (= s (unscramble (scramble s instrs) instrs)))))
+
+  (testing "decab using letter b"
+    (let [s "decab"
+          instrs ["rotate based on position of letter b"]]
+      (is (= s (unscramble (scramble s instrs) instrs)))))
+
+  (testing "first 3 instrs")
+    (let [s "fbgdceah"
+          instrs ["rotate based on position of letter a"
+                  "reverse positions 1 through 4"
+                  "rotate right 1 step"]]
+      ;bgdceahf
+      ;becdgahf
+      ;
+      (is (= "bgdceah" (unscramble s instrs)))))
+
+  (testing "decab"
+    (let [s "decab"
+          instrs ["swap position 4 with position 0"
+                  "swap letter d with letter b"
+                  "reverse positions 0 through 4"
+                  "rotate left 1 step"
+                  "move position 1 to position 4"
+                  "move position 3 to position 0"
+                  "rotate based on position of letter b"
+                  "rotate based on position of letter d"]]
+      (is (= "abcde" (unscramble s instrs))))))
