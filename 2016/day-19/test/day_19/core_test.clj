3ae2b918ec0dbf2996bba60b80d18ebc7a6d2d2f
John McConnell <johnnyillinois@gmail.com>
2017-01-22 19:17:33 -0800

    completed part 2: not optimal but still fast

--- 2016/day-19/test/day_19/core_test.clj
+++ 2016/day-19/test/day_19/core_test.clj
@@ -11,3 +11,19 @@
 
   (testing "with six elves"
     (is (= 5 (elf-with-presents (vec (repeat 6 1)))))))
+
+(deftest elf-with-presents-2-test
+  (testing "with four elves"
+    (is (= 1 (elf-with-presents-2 (vec (repeat 4 1))))))
+
+  (testing "with five elves"
+    (is (= 2 (elf-with-presents-2 (vec (repeat 5 1))))))
+
+  (testing "with six elves"
+    (is (= 3 (elf-with-presents-2 (vec (repeat 6 1))))))
+
+  (testing "with nine elves"
+    (is (= 9 (elf-with-presents-2 (vec (repeat 9 1))))))
+
+  (testing "with thirteen elves"
+    (is (= 4 (elf-with-presents-2 (vec (repeat 13 1)))))))
