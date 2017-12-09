5df853469b362e55343c46bd5ea6999cae430acb
John McConnell <john_mcconnell@apple.com>
2017-12-08 22:25:09 -0800

    day 9

--- 2017/day-9/src/day_9/core.clj
+++ 2017/day-9/src/day_9/core.clj
@@ -10,43 +10,95 @@
     (g-ls)
     (map #(clojure.string/split #"\s+"))))
 
-(defn score'
-  ([s i] (score' s i 1 nil))
-  ([s i d [tag & tags]]
-   (if (< i (count s))
-     (let [c (get s i)
-           i' (inc i)
-           c' (condp = c
-                \! :not
-                \< :open-gb
-                \> :close-gb
-                \{ :open-gr
-                \} :close-gr
-                :any)
-           push (fn [t2s & t1s]
-                  (concat (reverse t1s) t2s))]
-       (prn i (count s))
-       (condp = tag
-         nil (condp = c'
-               :open-gr (+ d (score' s i' (inc d) [:gr]))
-               :open-gb (score' s i' d [:gb])
-               :not (score' s i' d [:not])
-               (score' s i' d nil))
-         :gr (condp = c'
-               :close-gr (score' s i' (dec d) tags)
-               :open-gr (+ d (score' s i' (inc d) (push tags tag :gr)))
-               :open-gb (score' s i' d (push tags tag :gb))
-               :not (score' s i' d (push tags tag :not))
-               :any (score' s i' d (push tags tag)))
-         :gb (condp = c'
-               :close-gb (score' s i' d tags)
-               :open-gr (score' s i' d (push tags tag))
-               :close-gr (score' s i' d (push tags tag))
-               :open-gb (score' s i' d (push tags tag :gb))
-               :not (score' s i' d (push tags tag :not))
-               :any (score' s i' d (push tags tag)))
-         :not (score' s i' d tags)))
-     0)))
+(defn score''
+  [s]
+  (->>
+    (iterate
+      (fn
+        [[ttl i d [tag & tags]]]
+        (if (< i (count s))
+          (let [c (get s i)
+                i' (inc i)
+                c' (condp = c
+                     \! :not
+                     \< :open-gb
+                     \> :close-gb
+                     \{ :open-gr
+                     \} :close-gr
+                     :any)
+                push (fn [t2s & t1s]
+                       (concat (reverse t1s) t2s))]
+            ;(prn ttl i d tag c c')
+            (condp = tag
+              nil (condp = c'
+                    :open-gr [(+ ttl d) i' (inc d) [:gr]]
+                    :open-gb [ttl i' d [:gb]]
+                    :not [ttl i' d [:not]]
+                    [ttl i' d nil])
+              :gr (condp = c'
+                    :close-gr [ttl i' (dec d) tags]
+                    :open-gr [(+ ttl d) i' (inc d) (push tags tag :gr)]
+                    :open-gb [ttl i' d (push tags tag :gb)]
+                    :not [ttl i' d (push tags tag)]
+                    :any [ttl i' d (push tags tag)])
+              :gb (condp = c'
+                    :close-gb [ttl i' d tags]
+                    :open-gr [ttl i' d (push tags tag)]
+                    :close-gr [ttl i' d (push tags tag)]
+                    :open-gb [ttl i' d (push tags tag)]
+                    :not [ttl i' d (push tags tag :not)]
+                    :any [ttl i' d (push tags tag)])
+              :not [ttl i' d tags]))
+          [ttl]))
+      [0 0 1 nil])
+    (take-while #(> (count %) 2))
+    last
+    first))
+
+(defn score'''
+  [s]
+  (->>
+    (iterate
+      (fn
+        [[ttl i d [tag & tags]]]
+        (if (< i (count s))
+          (let [c (get s i)
+                i' (inc i)
+                c' (condp = c
+                     \! :not
+                     \< :open-gb
+                     \> :close-gb
+                     \{ :open-gr
+                     \} :close-gr
+                     :any)
+                push (fn [t2s & t1s]
+                       (concat (reverse t1s) t2s))]
+            ;(prn ttl i d tag c c')
+            (condp = tag
+              nil (condp = c'
+                    :open-gr [ttl i' (inc d) [:gr]]
+                    :open-gb [ttl i' d [:gb]]
+                    :not [ttl i' d [:not]]
+                    [ttl i' d nil])
+              :gr (condp = c'
+                    :close-gr [ttl i' (dec d) tags]
+                    :open-gr [ttl i' (inc d) (push tags tag :gr)]
+                    :open-gb [ttl i' d (push tags tag :gb)]
+                    :not [ttl i' d (push tags tag)]
+                    :any [ttl i' d (push tags tag)])
+              :gb (condp = c'
+                    :close-gb [ttl i' d tags]
+                    :open-gb [(inc ttl) i' d (push tags tag)]
+                    :open-gr [(inc ttl) i' d (push tags tag)]
+                    :close-gr [(inc ttl) i' d (push tags tag)]
+                    :not [ttl i' d (push tags tag :not)]
+                    :any [(inc ttl) i' d (push tags tag)])
+              :not [ttl i' d tags]))
+          [ttl]))
+      [0 0 1 nil])
+    (take-while #(> (count %) 2))
+    last
+    first))
 
 (defn -main
   "I don't do a whole lot ... yet."
