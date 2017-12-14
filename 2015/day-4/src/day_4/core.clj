adba5824092a2c6000a64e77b41a752c232e2c7e
John McConnell <john_mcconnell@apple.com>
2017-12-14 14:27:17 -0800

    day-4

--- 2015/day-4/src/day_4/core.clj
+++ 2015/day-4/src/day_4/core.clj
@@ -1,5 +1,20 @@
 (ns day-4.core
+  (:import
+    java.security.MessageDigest
+    java.math.BigInteger)
   (:gen-class))
+
+(defn md5 [s]
+  (let [algorithm (MessageDigest/getInstance "MD5")
+        raw (.digest algorithm (.getBytes s))]
+    (format "%032x" (BigInteger. 1 raw))))
+
+(defn first-hash
+  [s i x]
+  (let [h (md5 (str s i))]
+    (if (apply = \0 (take x h))
+      [i h]
+      (recur s (inc i) x))))
 
 (defn -main
   "I don't do a whole lot ... yet."
